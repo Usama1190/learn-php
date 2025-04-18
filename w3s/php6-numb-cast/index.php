9670bd5338335feb6741cc462284d6e7fcaa378c
Usama Israr <usamaisrar1190@gmail.com>
2025-04-18 17:30:42 +0500

    PHP numbers on the learning process

---
+++ w3s/php6-numb-cast/index.php
@@ -0,0 +1,73 @@
+<!-- <!DOCTYPE html>
+
+<html>
+    <body>
+        <?php 
+        $a = 34.3;
+        $b = 45;
+        $c = "24";
+        
+        var_dump($a);
+        echo "<br>";
+        var_dump($b);
+        echo "<br>";
+        var_dump($c); 
+        ?>
+
+        <p>Line breaks were added for better readibility.</p>
+    </body>
+</html> -->
+
+<!-- <!DOCTYPE html>
+
+<html>
+    <body>
+        <?php 
+        // Check if the type of a variable is integer
+        $x = 3473;
+        var_dump(is_int($x));
+
+        echo "<br>";
+
+        // Check again
+        $x = 45.76;
+        var_dump(is_int($x));
+        ?>
+    </body>
+</html> -->
+
+<!-- <!DOCTYPE html>
+
+<html>
+    <body>
+        <?php 
+        // Check if the type of a variable is float
+        $x = 45.345;
+        var_dump(is_float($x));
+        ?>
+    </body>
+</html> -->
+
+<!-- <!DOCTYPE html>
+
+<html>
+    <body>
+        <?php 
+        // Check if the numeric value is finite or infinite
+        $x = 1.9e411;
+        var_dump($x);
+        ?>
+    </body>
+</html> -->
+
+<!DOCTYPE html>
+
+<html>
+    <body>
+        <?php 
+        // Check if the numeric value is finite or infinite
+        $x = 1.9e411;
+        var_dump($x);
+        ?>
+    </body>
+</html>
